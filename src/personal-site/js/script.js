37eb38fa31229b65c68bbdc192e023ce2574236a
Aaron Steed <asteed7@gmail.com>
2016-08-01 23:22:16 +0100

    Make site more appealing  - Add background image to site at top  - All text and profile image centered on background image  - Add a random image from a selection to be the backround image on load  - Add Bower to the project to manage web dependencies  - Added animate.css via bower

---
+++ src/personal-site/js/script.js
@@ -0,0 +1,27 @@
+var hobbies = ["Web Developer", "Student of DCU",
+    "Book Reader", "Tea Drinker", "Software Engineer"
+];
+var imageUrl = "../resources/images/";
+var images = ["baloon-fest-baltinglass.jpeg", "carrigeen-sunset.jpeg",
+    "ha-penny-bridge.jpg", "ha-penny-bridge2.JPG",
+    "nou-camp-barcelona-pitch-view.jpeg",
+    "sagrada-familia-barcelona.JPG"
+];
+window.onload = function() {
+    var randomIndex = Math.floor((Math.random() * images.length));
+    console.log(randomIndex);
+    if (randomIndex === 1) {
+        document.getElementById("my-name").setAttribute("id", "my-name-black");
+        document.getElementById("what-I-do").setAttribute("id", "what-I-do-black");
+    }
+    document.getElementById("bg-image").setAttribute("src",
+        (imageUrl + images[randomIndex]));
+};
+
+// var index = 0;
+// function changeHobby() {
+// 	document.getElementById("hobby").innerHTMl = hobbies[index];
+// 	index++;
+// }
+
+// setInterval(changeHobby, 3000)
